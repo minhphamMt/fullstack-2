7f54dc8fb8a2405b46636a217052afea02ea76f0
minhthumt <minh280804@gmail.com>
2023-12-24 03:11:12 +0700

    frontend-24-12

--- src/containers/HomePage/section/Doctor.js
+++ src/containers/HomePage/section/Doctor.js
@@ -4,17 +4,75 @@ import { connect } from "react-redux";
 import "./Doctor.scss";
 import { FormattedMessage } from "react-intl";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import slider
+import "../../../../node_modules/slick-carousel/slick/slick.css";
+import "../../../../node_modules/slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 import * as actions from "../../../store/actions";
+import { LANGUAGES } from "../../../utils";
+import { changeLanguageApp } from "../../../store/actions";
 class MedicalFacility extends Component {
   constructor(props) {
     super(props);
+    // this.next = this.next.bind(this);
+    // this.previous = this.previous.bind(this);
     this.state = {
       Doctor: [],
       imgPreview: [],
+      showHide: false,
+      showHide1: true,
+      i: 0,
     };
   }
+  increase = (i) => {
+    let b = i + 1;
+    return b;
+  };
+
+  reduce = (i) => {
+    let b = i - 1;
+    return b;
+  };
+
+  next = () => {
+    let b = this.increase(this.state.i);
+
+    let x = this.state.Doctor.length - 5;
+
+    this.setState({
+      i: b,
+    });
+    this.slider.slickNext();
+    if (b > 0) {
+      this.setState({
+        showHide: true,
+      });
+    }
+    if (b === this.state.Doctor.length - 4) {
+      this.setState({
+        showHide1: false,
+      });
+    }
+  };
+  previous = () => {
+    let b = this.reduce(this.state.i);
+    this.setState({
+      i: b,
+    });
+    this.slider.slickPrev();
+
+    if (b === 0) {
+      this.setState({
+        showHide: false,
+      });
+    }
+    if (b > 0) {
+      this.setState({
+        showHide1: true,
+      });
+    }
+  };
   async componentDidMount() {
     this.props.loadDoctor();
   }
@@ -25,15 +83,16 @@ class MedicalFacility extends Component {
       });
     }
   }
-
+  changeLanguage = (language) => {
+    this.props.changeLanguageDoctor(language);
+  };
   render() {
     let Doctor = this.state.Doctor;
-    console.log(">>>Check state:", this.state.Doctor);
+    let language = this.props.language;
     let preview = [];
     if (this.state.Doctor) {
       let imgbase64 = "";
       let userChoose = Doctor;
-
       for (let i = 0; i < userChoose.length; i++) {
         if (userChoose[i].image) {
           imgbase64 = new Buffer(userChoose[i].image, `base64`).toString(
@@ -43,22 +102,50 @@ class MedicalFacility extends Component {
         }
       }
     }
-    console.log(">>>Check preview:", preview);
     let settings = {
       dots: false,
       infinite: true,
       speed: 500,
       slidesToShow: 4,
-      slidesToScroll: 2,
-    };
+      slidesToScroll: 1,
+      arrows: false,
+      // slickNext: false,
 
+      // className: "notes-slider",
+      // autoplay: true,
+      // autoplaySpeed: 1000,
+    };
     return (
       <>
         <div className="section-doctor">
           <div className="doctor-content">
-            <button className="see-more">see more</button>
-            <h2 className="title-doctor">Bác sĩ nổi bật tuần qua </h2>
-            <Slider {...settings}>
+            <button className="see-more">
+              {" "}
+              <FormattedMessage id="Doctor.see-more" />
+            </button>
+            <h2 className="title-doctor">
+              {" "}
+              <FormattedMessage id="Doctor.Best-choice" />{" "}
+            </h2>
+            {this.state.showHide1 === true && (
+              <button
+                onClick={() => this.next()}
+                className="slick-arrow  slick-next"
+              >
+                Next
+              </button>
+            )}
+
+            {this.state.showHide === true && (
+              <button
+                onClick={() => this.previous()}
+                className="slick-arrow slick-prev"
+              >
+                Previous
+              </button>
+            )}
+
+            <Slider ref={(c) => (this.slider = c)} {...settings}>
               {Doctor &&
                 Doctor.length > 0 &&
                 Doctor.map((item, index) => {
@@ -72,8 +159,12 @@ class MedicalFacility extends Component {
                             backgroundImage: `url(${preview[index]})`,
                           }}
                         ></div>
+
                         <p>
-                          {item.positionData.valueVi} {item.lastName} {""}
+                          {language === LANGUAGES.VI
+                            ? item.positionData.valueVi
+                            : item.positionData.valueEn}{" "}
+                          {item.lastName} {""}
                           {item.firstName} <br /> <span>doctor-content</span>
                         </p>
                       </div>
@@ -99,6 +190,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     loadDoctor: () => dispatch(actions.fetchLoadDoctor()),
+    changeLanguageDoctor: (language) => dispatch(changeLanguageApp(language)),
   };
 };
 
